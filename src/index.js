b5675b6e23beaf023077a25b18cc12616184c626
MathoVerse100 <mathoverse100@gmail.com>
2024-10-22 20:38:25 +0300

    Add updates files in sidebar, bard and main diectories

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import "./ui/main/mainLayout.css";
 import "./ui/main/board/boardLayout.css";
 import "./ui/main/sidebar/sidebarLayout.css";
 import "./ui/header/headerLayout.css";
-import { renderHeader } from "./ui/header/headerUpdates";
 import "./ui/header/mode/modeStyle.css";
 import "./ui/header/navigation/navigationStyle.css";
 import "./ui/header/title/titleStyle.css";
+import { renderHeader } from "./ui/header/headerUpdates";
 
 const header = document.querySelector("#header");
 renderHeader(header);
