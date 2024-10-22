7fbf3e1896845a56ef84393808740087e066cd60
MathoVerse100 <mathoverse100@gmail.com>
2024-10-22 19:02:23 +0300

    Separated style files and added basic styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import "./board.css";
-import "./header.css";
-import "./sidebar.css";
+import "./ui/global.css";
+import "./ui/main/mainLayout.css";
+import "./ui/main/board/boardLayout.css";
+import "./ui/main/sidebar/sidebarLayout.css";
+import "./ui/header/headerLayout.css";
